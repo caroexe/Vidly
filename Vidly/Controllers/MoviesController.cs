cda79f959af0fe0f1d895571b31ff82a3c246781
caroexe <carokyhn@gmail.com>
2022-03-23 16:48:55 +0100

    added new featues
    + added new featue Customers
    + added new feature Movies
    + added ActionLinks for Customers and Movies
    + added Details section for Movies and Customers

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -13,10 +13,11 @@ namespace Vidly.Controllers
         // GET: Movies/Random
         public ViewResult Random()
         {
-            var movie = new Movie() { Name = "Shrek!" };
+            var movie = new Movie { Name = "Random" };
+
             var customers = new List<Customer>
             {
-                new Customer { Name = "Customer 1"} ,
+                new Customer { Name = "Customer 1"},
                 new Customer { Name = "Customer 2"}
             };
 
@@ -29,5 +30,32 @@ namespace Vidly.Controllers
             return View(viewModel);
         }
 
+        // GET: Movies/
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+        
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek!" },
+                new Movie {Id = 2, Name = "Wall-E" }
+            };
+        }
+
+        // GET : Movies/Details
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
     }
 }
