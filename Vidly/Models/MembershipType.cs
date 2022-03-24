82a35aaee27a51fe6159045f282d0abaf07f3807
caroexe <carokyhn@gmail.com>
2022-03-24 13:13:54 +0100

    added MembershipTypeName
    + added and updated MembershipType with new column (Name)

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,8 @@ namespace Vidly.Models
 {
     public class MembershipType
     {
+        [StringLength(255)]
+        public string Name { get; set; }
         public byte Id { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
