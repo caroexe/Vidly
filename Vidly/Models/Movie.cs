fd1c365c79f43049a6023ee84c399276120ab8b7
caroexe <carokyhn@gmail.com>
2022-03-25 15:56:11 +0100

    added annotations to movie.cs
    + added a few Display Name annotations to movie.cs

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -16,9 +16,14 @@ namespace Vidly.Models
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Date when added")]
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
 }
