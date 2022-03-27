46e4943dd8cfe6cf78435563c94c033cfdbccbc6
caroexe <carokyhn@gmail.com>
2022-03-27 18:41:51 +0200

    added Validation for Movie Form
    + added Validation for Movie Form

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -48,6 +48,7 @@ namespace Vidly.Controllers
             var genre = _context.Genres.ToList();
             var viewModel = new MovieFormViewModel
             {
+                Movie = new Movie(),
                 Genres = genre
             };
 
@@ -56,8 +57,19 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel()
+                {
+                    Movie = movie,
+                    Genres = _context.Genres.ToList()
+                };
+                return View("MovieForm", viewModel);
+            }
+
             if (movie.Id == 0)
             {
                 var currentDateTime = DateTime.Now;
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,20 +9,25 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
+        
         public Genre Genre { get; set; }
 
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
 
+        [Required]
         [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
         [Display(Name = "Date when added")]
         public DateTime DateAdded { get; set; }
 
+        [Required]
+        [Range(1,20,ErrorMessage = "Number in Stock has to be between 1 and 20")]
         [Display(Name = "Number in Stock")]
         public int NumberInStock { get; set; }
     }
