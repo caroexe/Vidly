937a2106724243d31c501e398f722e2e2c3c8c31
caroexe <carokyhn@gmail.com>
2022-03-28 11:46:42 +0200

    Update Global.asax and installed new packages
    +Updated Global.asax
    +installed new packages

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
+using System.Web.Routing;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +14,7 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
